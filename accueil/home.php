5e5ad72e6fafe562dc7444907ace190545440b43
clemencebric <markerbic3@gmail.com>
2024-03-11 13:04:48 +0100

    feat(view): ajout de la navbar

---
+++ accueil/home.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'Acceuil</title>
+    <link rel="stylesheet" href="./accueil.css">
+
+</head>
+<body>
+    <nav class="navbar">
+        <img src="./quizzeo-sans-fond.png"  alt='logo' class='logo'/>
+        <div class='desktopMenu'>
+            <a href="#" class="desktopMenuListItem">Home</a> <!-- a href pour redirection pages -->
+            <a href="./attractions.php" class="desktopMenuListItem">Attractions</a>
+            <a href="./pagefavoris.php" class="desktopMenuListItem">Favoris</a>
+            <a href="deconnexion.php" class="desktopMenuListItem">Connexion</a>
+        </div>
+        <!--<p> <span class="pastille"></span> connecté </p>-->
+
+    </nav>
+
+    <h1>BIENVENUE SUR QUIZZEO <!--<?php echo $identifiant; ?>--> !</h1><!--Personnalisation de la session -->
+    
+    
+</body>
+</html>
