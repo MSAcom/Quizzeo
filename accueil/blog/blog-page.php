aa585cc643a39b369f604f128946cee317535c1e
MCgit20 <m.chen1@ecole-ipssi.net>
2024-03-17 17:18:30 +0100

    feat(view):modification page blog

---
+++ accueil/blog/blog-page.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog - Mon Site</title>
+    <link rel="stylesheet" href="blog.css">
+</head>
+<body>
+    <header>
+        <h1>Mon Blog</h1>
+        <nav class="navbar">
+            <img src="../accueil/quizzeo-sans-fond.png" height="50" alt='logo' class='logo'/>
+            <div class="desktopMenu">
+                <a href="index.php">Accueil</a>
+                <a href="blog.php">Blog</a>
+                <a href="../accueil/profil.php">Profil</a>
+                <a href="../accueil/deconnexion.php">Déconnexion</a>
+            </div>
+        </nav>
+    </header>
+    
+    <main>
+        <section class="articles">
+            <article>
+                <h2>Culture générale</h2>
+                <p>Testez votre culture generale, plusieurs type de quizz accesible pour les enfants et les adultes</p>
+                <p>C'est à vous de jouer !</p>
+                <a href="https://www.laculturegenerale.com/60-tests-de-culture-generale1/">Lire la suite</a>
+            </article>
+            <article>
+                <h2>L'histoire </h2>
+                <p>À quoi servent les quizz, quel est l'origine du mot quizz ?</p>
+                <p>Découvrons emsemble !</p>
+                <a href="https://fr.wikipedia.org/wiki/Quiz">Lire la suite</a>
+            </article>
+            <!-- Ajoutez d'autres articles ici -->
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2024 Mon blog. All reserved.</p>
+    </footer>
+</body>
