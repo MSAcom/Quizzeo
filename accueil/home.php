d043adea1380f8288c6ea7e4cd0beb95b0a22a50
Arame28 <na.thiaw@ecole-ipssi.net>
2024-03-21 16:05:35 +0100

    modification

--- accueil/home.php
+++ accueil/home.php
@@ -9,12 +9,13 @@
 <body>
     <nav class="navbar">
         <img src="../quizz/images/quizzeo-sans-fond.png"  alt='logo' class='logo'/>
-        <div class='desktopMenu'>
-            <a href="#" class="desktopMenuListItem">Home</a> <!-- a href pour redirection pages -->
-            <a href="blog-page.php" class="desktopMenuListItem">Blog</a>
-            <a href="inscription.php" class="desktopMenuListItem">Inscription</a>
-            <a href="connexion.php" class="desktopMenuListItem">Connexion</a>
-        </div>
+        <div class='destopMenu'>
+            <a href="#">Home</a></li>
+            <a href="blog-page.php" class="destopMenuListItem">Blog</a>
+            <a href="inscription.php" class="destopMenuListItem">Inscription</a>
+            <a href="connexion.php" class="destopMenuListItem">Connexion</a>
+        
+    </div>
     </nav>
 </body>
 </html>
